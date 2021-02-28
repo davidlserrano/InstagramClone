be4034051414abdb9d4ec9e36405f8ed3dba2bb0
David Serrano <davidserrano@Davids-iMac.attlocal.net>
2021-02-27 16:49:58 -0800

    basic app set up

---
+++ InstagramClone/app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "loginBtn onClick");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "login attempt with: "+ username);
+
+    }
+
+
+}
